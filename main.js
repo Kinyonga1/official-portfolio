a7ed793ce3b10c3aa1fbfade4f42f09478b53e8b
van09 <lytasvan@gmail.com>
2024-06-23 04:27:43 +0300

    version23

--- main.js
+++ main.js
@@ -214,3 +214,26 @@ document.getElementById('fname').addEventListener('input', function (e) {
     .slice(0, 3)
     .join('-');
 });
+
+
+/**
+ * BACK TO TOP BUTTON
+ */
+
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+  // visible back top btn when scrolled 5% of the page
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
